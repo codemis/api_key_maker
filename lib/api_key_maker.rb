ad452f82a0d442029808f4042ff7bacdd589c875
Codemis <johnathan@still-water.com>
2012-01-26 15:50:13 -0800

    Adding documentation.

--- lib/api_key_maker.rb
+++ lib/api_key_maker.rb
@@ -5,7 +5,6 @@ require "digest/sha1"
 
 module ApiKeyMaker
   
-  # Create an API Key
   def self.make_api_key(length = 10)
     Digest::SHA1.hexdigest(Time.now.to_s + rand(12341234).to_s)[1..length]
   end
--- lib/api_key_maker/model_additions.rb
+++ lib/api_key_maker/model_additions.rb
@@ -1,6 +1,15 @@
 module ApiKeyMaker
   module ModelAdditions
-    
+    # To have an attribute default to a SHA1 encrypted unique string,
+    # call <tt>make_api_key</tt> in any Active Record model class, and pass
+    # it the attribute, and the length of string.  Length is optional, and
+    # defaults to 10 characters.  The maximum characters is 39.
+    #
+    #     class ApiAccess < ActiveRecord::Base
+    #       make_api_key :api_token, 13
+    #     end
+    #
+    # This will be triggered on the <tt>before_validation</tt> callback.
     def make_api_key(attribute, length = 10)
       before_validation do
         send("#{attribute}=", ApiKeyMaker.make_api_key(length))
