03931f6216fcec8f7f1899a6c3bcc6360a7a7dfd
Codemis <johnathan@still-water.com>
2012-01-26 11:53:02 -0800

    Added functionality to make an api key.

--- lib/api_key_maker.rb
+++ lib/api_key_maker.rb
@@ -1,10 +1,11 @@
 require "api_key_maker/version"
+require "digest/sha1"
 
 module ApiKeyMaker
   
   # Create an API Key
-  def make_key()
-    return ""
+  def self.make_api_key(length = 10)
+    Digest::SHA1.hexdigest(Time.now.to_s + rand(12341234).to_s)[1..length]
   end
   
 end
--- spec/api_key_maker_spec.rb
+++ spec/api_key_maker_spec.rb
@@ -7,4 +7,12 @@ describe ApiKeyMaker do
     ApiKeyMaker.make_api_key.empty?.should be_false
   end
   
+  it "should create an api key to a set length" do
+    ApiKeyMaker.make_api_key(5).length.should eq(5)
+  end
+  
+  it "should create an api key to a default length" do
+    ApiKeyMaker.make_api_key.length.should eq(10)
+  end
+  
 end
